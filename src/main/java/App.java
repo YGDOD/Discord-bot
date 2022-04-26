233477bf5b351854e4bbebb072f55eb224990aed
YGDOD <104467167+YGDOD@users.noreply.github.com>
2022-04-27 00:47:53 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,10 @@
 import info.help;
 import net.dv8tion.jda.api.JDABuilder;
-import tests.buttons;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-        JDABuilder jda = JDABuilder.createDefault("OTE4NTg5MjkwNzczMDk0NDEw.YbJc8Q.Nd2J-VWQbSaG4edm95IeDT_K6H4");
-        jda.addEventListeners(new buttons());
+        JDABuilder jda = JDABuilder.createDefault("OTE4NTg5MjkwNzczMDk0NDEw.YbJc8Q.diZxsAcalMVzVs2F616ChZ_Cff4");
         jda.addEventListeners(new help());
         jda.build();
     }
